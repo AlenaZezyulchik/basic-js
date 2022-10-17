4db328f5e368412d80b01863a812a96d2ea8adfa
Aliaksandr Ziaziulchyk <aliaksandr_ziaziulchyk@epam.com>
2022-10-17 23:10:41 +0300

    feat:added encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+    let countItem = 1;
+    let encodeResult = '';
+    if (str == '') {
+        return "";
+    };
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] != str[i]) {
+            encodeResult += (countItem == 1 ? '' : countItem) + str[i - 1];
+            countItem = 1;
+        } else countItem++;
+    }
+    encodeResult += (countItem == 1 ? '' : countItem) + str[str.length - 1];
+    return encodeResult;
 }
 
 module.exports = {
-  encodeLine
+    encodeLine
 };
