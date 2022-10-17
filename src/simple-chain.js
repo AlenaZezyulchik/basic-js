592d8e7a813a659f3fb50d4e588b6c8a8075895c
Aliaksandr Ziaziulchyk <aliaksandr_ziaziulchyk@epam.com>
2022-10-17 22:52:03 +0300

    feat:added code seven tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(i) {
+        this.chain.push(i);
+        return this;
+    },
+    removeLink(position) {
+        if (typeof(position) !== 'number') {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+
+        position--;
+        if (position < 0 || position >= this.getLength()) {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+
+        this.chain.splice(position, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let str = '';
+
+        for (let i = 0; i < this.getLength(); ++i) {
+            str += `( ${this.chain[i]} )`;
+
+            if (i < this.getLength() - 1) {
+                str += '~~';
+            }
+        }
+
+        this.chain = [];
+        return str;
+    }
+}
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+
+    console.log(Object.getOwnPropertyNames(date))
+    if (!date.getTime) {
+        throw new Error('Invalid date!');
+    }
+
+    if (Object.getOwnPropertyNames(date).length > 0) {
+        throw new Error('Invalid date!');
+    }
+
+    const months = {
+        autumn: [8, 9, 10],
+        winter: [11, 0, 1],
+        spring: [2, 3, 4],
+        summer: [5, 6, 7]
+
+    }
+
+    let month = date.getMonth();
+    let result;
+    for (const key in months) {
+        if (months[key].indexOf(month) !== -1) result = key;
+    }
+    return result
 }
 
+
 module.exports = {
-  getSeason
+    getSeason
 };
