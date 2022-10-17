f9de2dc4386cc622594fe77f9bde90964a9a2025
Aliaksandr Ziaziulchyk <aliaksandr_ziaziulchyk@epam.com>
2022-10-17 22:59:39 +0300

    feat:added extended-repeater and vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    let result = [];
+
+    if (options.separator == undefined) options.separator = '+';
+    if (options.addition === undefined) options.addition = '';
+    if (options.addition == null) options.addition = 'null';
+    if (options.repeatTimes == undefined) options.repeatTimes = 1;
+    if (options.additionSeparator === undefined) options.additionSeparator = '|';
+    if (options.additionRepeatTimes == undefined) options.additionRepeatTimes = 1;
+
+    for (let i = 0; i < options.repeatTimes; i++) {
+        let prStr = [];
+        for (let j = 0; j < options.additionRepeatTimes; j++) {
+            prStr.push(options.addition);
+        }
+        result.push(str + prStr.join(options.additionSeparator));
+    }
+
+    return result.join(options.separator);
 }
 
 module.exports = {
-  repeater
+    repeater
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(bool) {
+        this.bool = bool;
+    }
+    encrypt(str, key) {
+        let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (str && key) {
+            str = str.toUpperCase();
+            key = key.toUpperCase();
+            let count = 0;
+            let array = str.split(' ');
+            let newResult = array.map((item) => {
+                let result = '';
+                for (let i = 0; i < item.length; i++) {
+                    if (alphabet.indexOf(item[i]) != -1) {
+                        let indexOne = alphabet.indexOf(item[i]);
+                        if (!key[count]) count = 0;
+                        let indexTwo = alphabet.indexOf(key[count]);
+                        count++;
+                        if ((indexOne + indexTwo) > 25) {
+                            result += alphabet[indexOne + indexTwo - alphabet.length];
+                        } else {
+                            result += alphabet[indexOne + indexTwo];
+                        }
+                    } else result += item[i];
+                }
+                if (this.bool == false) {
+                    result = result.split('').reverse().join('');
+                }
+                return result
+            });
+            if (this.bool == false) return newResult.reverse().join(' ');
+            return newResult.join(' ');
+        } else {
+            throw new Error("Incorrect arguments!");
+        }
+    }
+    decrypt(cypher, key) {
+        let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (cypher && key) {
+            key = key.toUpperCase();
+            let arr = cypher.split(' ');
+
+            let count = 0;
+            let newResult = arr.map((item) => {
+                let result = '';
+                for (let i = 0; i < item.length; i++) {
+                    if (!key[count]) count = 0;
+                    let indexKey = alphabet.indexOf(key[count]);
+                    if (alphabet.indexOf(item[i]) != -1) {
+                        let indexCypher = alphabet.indexOf(item[i]);
+                        count++;
+                        let index = Math.abs(indexCypher + alphabet.length - indexKey);
+                        if (index > 25) index = index - alphabet.length;
+                        result += alphabet[index];
+                    } else result += item[i];
+                }
+                if (this.bool == false) {
+                    result = result.split('').reverse().join('');
+                }
+                return result;
+            });
+            if (this.bool == false) return newResult.reverse().join(' ');
+            return newResult.join(' ');
+        } else {
+            throw new Error("Incorrect arguments!");
+        }
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
